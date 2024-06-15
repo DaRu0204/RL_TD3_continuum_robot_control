7c5528aae132aa2ce4d5a292acb87f87d1e9b29e
Ivan <ivan.virgala@tuke.sk>
2024-06-15 20:07:26 +0200

    render_ok

--- full.py
+++ full.py
@@ -184,15 +184,14 @@ class ContinuumRobotEnv:
         initial_state = np.zeros(self.state_dim)
         self.state = initial_state
         return initial_state
-
+    
     def step(self, actions):
         self.current_step += 1
-        #actions = np.clip(actions, -self.max_tendon_tension, self.max_tendon_tension)
-        #actions = actions * self.max_action
+        actions = np.clip(actions, -self.max_tendon_tension, self.max_tendon_tension)
         next_state = self._simulate_robot(actions)
         reward = self._compute_reward(next_state)
         done = self.current_step >= self.max_steps
-        self.state = next_state
+        self.state = next_state  # Update the environment's state
         return next_state, reward, done
 
     def _simulate_robot(self, actions):
@@ -230,13 +229,11 @@ class ContinuumRobotEnv:
         return reward
 
     def render(self, actions=None):
-        if actions is not None:
-            simulated_state = self._simulate_robot(actions)
-        else:
-            segment_positions = np.zeros((2, self.num_segments + 1))
-
         segment_positions = [np.zeros(2)]
         orientation = 0.0
+
+        if actions is not None:
+            actions = np.clip(actions, -self.max_tendon_tension, self.max_tendon_tension)
 
         for i in range(self.num_segments):
             kappa = actions[i] / self.segment_length if actions is not None else 0
@@ -260,7 +257,7 @@ class ContinuumRobotEnv:
                 segment_positions.append(start_pos + np.array([delta_x, delta_y]))
 
         segment_positions = np.array(segment_positions)
-        print("segment_positions:",segment_positions)
+        print("segment_positions:", segment_positions)
         plt.figure(figsize=(8, 6))
         plt.plot(segment_positions[:, 0], segment_positions[:, 1], color='blue', label='Robot Curve')
         plt.scatter(0, 0, color='black', label='Base Position')
@@ -295,7 +292,7 @@ wandb.init(
 
 def main():
     # Training loop
-    total_episodes = 100
+    total_episodes = 1000
     rewards = []
     batch_size = 64
     replay_buffer = ReplayBuffer(buffer_size=1000000)
@@ -340,7 +337,7 @@ def main():
     print("Error:", error)
     
     # Render the environment based on the updated state
-    env.render()
+    env.render(actions=actionn)
 
 if __name__ == "__main__":
     main()
